0140d38b5305b0587a8b3c3cebb5a1de8548e75a
leonCaptain <leon522886@gmail.com>
2019-03-27 20:49:19 +0800

    submit portfolio interface

--- back-end/src/controller/PortfolioCoinController.ts
+++ back-end/src/controller/PortfolioCoinController.ts
@@ -7,16 +7,18 @@ export class PortfolioCoinController {
   private portfolioCoinRepository = getRepository(PortfolioCoin);
 
   async all(request: Request, response: Response, next: NextFunction) {
-    return this.portfolioCoinRepository.find({ where: { portfolioID: request.params.pid, isDelete: StartOrClose.START} });
+   return this.portfolioCoinRepository.find({ where: { portfolioID: request.params.pid} });
+
+   // return this.portfolioCoinRepository.find({ where: { portfolioID: request.params.pid, isDelete: StartOrClose.START} });
   }
 
   async save(request: Request, response: Response, next: NextFunction) {
-    return this.portfolioCoinRepository.save(request.body);
+    const portfolioCoin = this.portfolioCoinRepository.create(request.body);
+    return this.portfolioCoinRepository.save(portfolioCoin);
   }
 
   async remove(request: Request, response: Response, next: NextFunction) {
     let portfolioCoinToRemove = await this.portfolioCoinRepository.findOne(request.params.pcid);
-    portfolioCoinToRemove.isDelete = StartOrClose.CLOSE;
-    await this.portfolioCoinRepository.save(portfolioCoinToRemove);
+    await this.portfolioCoinRepository.remove(portfolioCoinToRemove);
   }
 }
--- back-end/src/entity/PortfolioCoin.ts
+++ back-end/src/entity/PortfolioCoin.ts
@@ -26,18 +26,19 @@ export default class PortfolioCoin extends BaseEntity {
     @Column("double", { name: "initial_investment"})
     initialInvestment: number;
 
-    @Column("double", { name: "current_price"})
-    currentPrice: number;
+    // @Column("double", { name: "current_price"})
+    // currentPrice: number;
 
-    @Column("double", { name: "mirror_price"})
-    mirrorPrice: number;
-
+    // @Column("double", { name: "mirror_price"})
+    // mirrorPrice: number;
+    
+    //OneToOne
     @Column("int", { name: "project_id"})
     projectID: number;
 
-    @Column("double", { name: "mirror_profit"})
-    mirrorProfit: number;
+    // @Column("double", { name: "mirror_profit"})
+    // mirrorProfit: number;
 
-    @Column("double", { name: "mirror_profit_rate"})
-    mirrorProfitRate: number;
+    // @Column("double", { name: "mirror_profit_rate"})
+    // mirrorProfitRate: number;
 }
